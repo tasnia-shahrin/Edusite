4210f2d8d56b78d368c4b7d48b0dc37f27f9d2b7
tasnia-shahrin <tasniashahrin269@gmail.com>
2024-09-12 00:20:18 +0600

    mon kharap

---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Programs.css'
+import program_1 from '../../assets/program-1.jpg'
+import program_2 from '../../assets/program-2.jpg'
+import program_3 from '../../assets/program-3.jpg'
+
+
+const Programs = () => {
+  return (
+    <div className='Programs container'>
+        <div className="program">
+            <img src={program_1} alt="" />
+        </div>
+        <div className="program">
+            <img src={program_2} alt="" />
+        </div>
+        <div className="program">
+            <img src={program_3} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Programs
